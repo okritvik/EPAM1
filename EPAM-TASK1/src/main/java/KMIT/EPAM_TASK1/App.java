5f3a3e71bacceb07fa47c1bf7d61f27072019578
okritvik <okritvik@gmail.com>
2019-03-11 12:48:52 +0530

    First Commit

---
+++ EPAM-TASK1/src/main/java/KMIT/EPAM_TASK1/App.java
@@ -0,0 +1,13 @@
+package KMIT.EPAM_TASK1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
